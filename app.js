86379928cda2f0606051e1afd78d870ce36c28a0
Andrew Souza <drewmargielaa@gmail.com>
2020-09-10 17:38:50 -0400

    added color red to numbers under 0, and green greater than 0

--- app.js
+++ app.js
@@ -8,14 +8,21 @@ const btns = document.querySelectorAll(".btn");
 btns.forEach(function (btn) {
     btn.addEventListener("click", function(e) {
         const styles = e.currentTarget.classList;
-        if (styles.contains("decrease")){
+        if (styles.contains("decrease")) {
             count--;
-        }
-        else if(styles.contains('increase')){
+        } else if(styles.contains("increase")) {
             count++;
-        }
-        else {
+        } else {
             count = 0;
+        }
+        if(count > 0){
+            value.style.color = "green";
+        }
+        if(count < 0) {
+            value.style.color = "red";
+        }
+        if(count === 0) {
+            value.style.color = "#222"
         }
         value.textContent = count;
     });
